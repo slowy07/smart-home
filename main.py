a288104050ec49f7d249922c8b5aa9541b1b66e6
slowy07 <slowy.arfy@gmail.com>
2021-07-16 21:58:20 +0700

    clean up code

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ getpar = BeautifulSoup(web.text, "html.parser")
 location = getpar.find("h1", class_="CurrentConditions--location--2_osB").text
 weather_info = getpar.find("div", class_="CurrentConditions--phraseValue--17s79").text
 temperature = getpar.find("div", class_="CurrentConditions--primary--39Y3f").span.text
-
+prediction = getpar.find("div", class_="CurrentConditions--precipValue--1RgXi").span.text
 
 print(type(location))
 print(type(temperature))
+print(type(prediction))

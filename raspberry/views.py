77bd010ef855e33b8f10467feb72e1b6a336e9f6
slowy07 <slowy.arfy@gmail.com>
2021-07-25 10:05:54 +0700

    removed unused imported modules

--- raspberry/views.py
+++ raspberry/views.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rooms.models import Device
 
 from raspberry.models import GPIO_pins
